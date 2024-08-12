8f40ab440b5efbcd27c0b71de7bda380197d57bc
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-12 10:58:57 +0900

    Feat: 추가 새로운 파일 추가
    todolist에 필요한 pages파일들 추가

---
+++ recoil-tailwind-test1/src/pages/TodoItemCreator.tsx
@@ -0,0 +1,34 @@
+import {ChangeEventHandler, useCallback, useState} from "react";
+import {useSetRecoilState} from "recoil";
+import {todoListstate} from "./todoListState";
+
+export default function TodoItemCreator() {
+    const [inputValue, setInputValue] = useState('');
+    const setTodoList = useSetRecoilState(todoListstate);
+    const addItem = useCallback(() => {
+        setTodoList((oldTodoList) => [
+            ...oldTodoList,
+            {
+                id: getId(),
+                text: inputValue,
+                isComplete: false,
+            }
+        ]);
+        setInputValue('');
+    } ,[inputValue, setTodoList])
+    const onChange: ChangeEventHandler<HTMLInputElement> = useCallback(({ target: {value}}) => {
+        setInputValue(value);
+    },[]);
+
+    return (
+        <div>
+            <input type='text' value={inputValue} onChange={onChange}/>
+            <button onClick={addItem}>추가</button>
+        </div>
+    )
+}
+
+let id = 0;
+function getId() {
+    return id++;
+}
---
+++ recoil-tailwind-test1/src/pages/TodoList.tsx
@@ -0,0 +1,18 @@
+import { useRecoilValue } from "recoil";
+import { todoListstate } from "./todoListState";
+import TodoItemCreater from './TodoItemCreator';
+import TodoItem from './TodoItem';
+
+export default function TodoList() {
+    const todolist=useRecoilValue(todoListstate)
+    return(
+        <div>
+            <TodoItemCreater />
+            {
+                todolist.map((todoItem,index)=>(
+                    <TodoItem key={index} item={todoItem} />
+                ))
+            }
+        </div>
+    )
+}
