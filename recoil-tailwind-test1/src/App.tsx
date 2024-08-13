90d67f4ebcbaeb81334aa8bb040a3d21c0428bb4
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-13 16:11:05 +0900

    Feat: 추가 라우터 추가
    환전 기능을 위한 /exchange 라우터 추가

--- recoil-tailwind-test1/src/App.tsx
+++ recoil-tailwind-test1/src/App.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TodoList from './pages/TodoList';
-import NavBar from "./components/NavBar";
+
+import Home from "./pages/Home";
+import Main from './components/Main';
+import TodoList from "./todo/TodoList";
+
+import Header from "./components/Header";
+import { Login } from "./pages/Login";
+import LoginSuccess from './pages/LoginSuccess';
+
+import Exchange from "./components/Exchange";
+
 
 function App() {
   return (
     <RecoilRoot>
-      <Router>
-        <NavBar />
+      <>
+        
         <Routes>
-          <Route path="/todolist" element={<TodoList />} />
-        </Routes>
-      </Router>
+          <Route path="/" element={<Main />} />
+          <Route path="/todo" element={<TodoList />} />
+          <Route path="/home" element={<Home />} /> 
+          <Route path="/header" element={<Header />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/loginsuccess" element={<LoginSuccess />} />
+          <Route path="/exchange" element={<Exchange />} />
+        </Routes> 
+      </>
     </RecoilRoot>
   );
 }
