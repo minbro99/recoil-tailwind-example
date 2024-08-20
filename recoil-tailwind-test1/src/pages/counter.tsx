35ded599ed5b7aefe45d06e6c9831b4ddd3edd0f
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-20 11:43:19 +0900

    Feat:  추가 버튼별로 누르면 값이 변경
    입력값에 대해 더하기 버튼을 누르면 더하기가 빼기 버튼을 누르면 빼기가 되게 코드 작성

--- recoil-tailwind-test1/src/pages/counter.tsx
+++ recoil-tailwind-test1/src/pages/counter.tsx
@@ -1,16 +1,23 @@
 import React, { ChangeEvent } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { plusState, resultState } from "../states/CounterState";
+import { plusState, plusResultState, minusResultState } from "../states/CounterState";
 
 const Counter: React.FC = () => {
-  const [plus, setPlus] = useRecoilState(plusState);
-  const result = useRecoilValue(resultState);
+  const [inputValue, setInputValue] = useRecoilState(plusState); // 하나의 입력 값을 관리
+  const result1 = useRecoilValue(plusResultState);
+  const result2 = useRecoilValue(minusResultState);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPlus(Number(e.target.value));  // 입력 값을 숫자로 변환하여 상태 업데이트
+    setInputValue(Number(e.target.value)); // 입력 값을 숫자로 변환하여 상태 업데이트
   };
 
-  const increment = () => setPlus(plus + 1);
+  const increment = () => {
+    setInputValue(inputValue + 1); 
+  };
+
+  const decrement = () => {
+    setInputValue(inputValue - 1); 
+  };
 
   return (
     <div className="text-center bg-blue-100">
@@ -20,7 +27,7 @@ const Counter: React.FC = () => {
           Current Value:
           <input
             type="text"
-            value={plus}
+            value={inputValue}
             onChange={handleInputChange}
             className="ml-2 border rounded px-2 py-1 bg-gray-300"
           />
@@ -30,9 +37,16 @@ const Counter: React.FC = () => {
         className="bg-red-100 text-green-800 mt-4 px-4 py-2 rounded"
         onClick={increment}
       >
-        증감
+        더하기
       </button>
-      <p>결과값 (입력값 + 1): {result}</p>
+      <p>결과값 (입력값 + 1): {result1}</p>
+      <button
+        className="bg-red-100 text-green-800 mt-4 px-4 py-2 rounded"
+        onClick={decrement}
+      >
+        빼기
+      </button>
+      <p>결과값 (입력값 - 1): {result2}</p>
     </div>
   );
 };
