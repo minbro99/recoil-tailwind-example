27a3c44cbfecbd665cd09b1072bfcb745f1ac23a
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-20 10:46:24 +0900

    Feat: 추가 counter에 숫자 입력하고 버튼 클릭하면 반응하게 변경
    counter에 숫자 입력하고 버튼 클릭하면 반응하게 변경

--- recoil-tailwind-test1/src/pages/counter.tsx
+++ recoil-tailwind-test1/src/pages/counter.tsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { plusState, resultState } from "../states/CounterState"; // state 파일에서 plusState와 resultState를 가져옵니다.
+import { plusState, resultState } from "../states/CounterState";
 
 const Counter: React.FC = () => {
-  // plusState는 값을 직접 수정할 수 있는 상태입니다.
   const [plus, setPlus] = useRecoilState(plusState);
-  
-  // resultState는 plusState에 기반한 파생된 값이지만, set을 통해 직접 수정할 수도 있습니다.
   const result = useRecoilValue(resultState);
-  
-  // 증가 버튼을 클릭했을 때 plusState를 증가시킵니다.
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPlus(Number(e.target.value));  // 입력 값을 숫자로 변환하여 상태 업데이트
+  };
+
   const increment = () => setPlus(plus + 1);
 
   return (
-    <div className="text-center">
+    <div className="text-center bg-blue-100">
       <h1 className="text-3xl">계산기</h1>
-      <p>Current Value: {plus}</p>
-      <p>Result Value (plus + 1): {result}</p>
-      <button onClick={increment} className="bg-black text-white ">Increment</button>
+      <p>
+        <label>
+          Current Value:
+          <input
+            type="text"
+            value={plus}
+            onChange={handleInputChange}
+            className="ml-2 border rounded px-2 py-1 bg-gray-300"
+          />
+        </label>
+      </p>
+      <button
+        className="bg-red-100 text-green-800 mt-4 px-4 py-2 rounded"
+        onClick={increment}
+      >
+        증감
+      </button>
+      <p>결과값 (입력값 + 1): {result}</p>
     </div>
   );
 };
