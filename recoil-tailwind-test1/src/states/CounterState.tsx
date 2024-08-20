9cd393361af5d5749a80d625f29d0813cf5b1745
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-20 13:27:41 +0900

    Feat: 추가 곱셈, 나눗셈에대한 states 추가
    곱셈 나눗셈에 대한 states 추가

--- recoil-tailwind-test1/src/states/CounterState.tsx
+++ recoil-tailwind-test1/src/states/CounterState.tsx
@@ -1,4 +1,4 @@
-import { atom,selector } from "recoil";
+import { atom, selector } from "recoil";
 
 export const plusState = atom<number>({
     key: "plusState",
@@ -8,28 +8,62 @@ export const plusState = atom<number>({
 export const minusState = atom<number>({
     key: "minusState",
     default: 0,
-})
+});
+
+export const multipleState = atom<number>({
+    key: "multipleState",
+    default: 0,
+});
+
+export const divideState = atom<number>({
+    key: "divideState",
+    default: 0,
+});
 
 export const plusResultState = selector<number>({
     key: "plusResultState",
     get: ({ get }) => {
-        const plus = get(plusState);  
-        return plus + 1 ;  
+        const plus = get(plusState);
+        return plus + 1;
     },
     set: ({ set }, result1) => {
-        const newPlusValue = (result1 as number) +1; 
-        set(plusState, newPlusValue);  
-      },
+        const newPlusValue = (result1 as number) + 1;
+        set(plusState, newPlusValue);
+    },
 });
 
 export const minusResultState = selector<number>({
     key: "minusResultState",
     get: ({ get }) => {
-        const minus = get(minusState);  
-        return minus - 1 ;  
+        const minus = get(minusState);
+        return minus - 1;
     },
     set: ({ set }, result2) => {
-        const newMinusValue = (result2 as number) -1; 
-        set(minusState, newMinusValue);  
-      },
-})
+        const newMinusValue = (result2 as number) - 1;
+        set(minusState, newMinusValue);
+    },
+});
+
+export const multipleResultState = selector<number>({
+    key: "multipleResultState",
+    get: ({ get }) => {
+        const multiple = get(multipleState);
+        return multiple * 2;
+    },
+    set: ({ set }, result3) => {
+        const newMultipleValue = (result3 as number) * 2;
+        set(multipleState, newMultipleValue);
+    },
+});
+
+export const divideResultState = selector<number>({
+    key: "divideResultState",
+    get: ({ get }) => {
+        const divide = get(divideState);
+        return divide / 2;
+    },
+    set: ({ set }, result4) => {
+        const newDivideValue = (result4 as number) / 2;
+        set(divideState, newDivideValue);
+    },
+});
