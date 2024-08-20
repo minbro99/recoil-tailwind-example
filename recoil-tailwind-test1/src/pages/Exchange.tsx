dc9543f31aa71a8ebca6f6f33f3056a0b736ec13
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-20 14:04:15 +0900

    Feat: components에서 파일 이동
    compontes에 있던 파일을 pages로 이동

---
+++ recoil-tailwind-test1/src/pages/Exchange.tsx
@@ -0,0 +1,62 @@
+// components/DollarConverter.tsx
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { wonState, dollarState, enhwaState, poundState } from "../states/exchnageState";
+
+const Exchange: React.FC = () => {
+  const [won, setWon] = useRecoilState<number>(wonState); // 원화 상태
+  const dollar = useRecoilValue<number>(dollarState);     // 달러 상태
+  const enhwa = useRecoilValue<number>(enhwaState);
+  const pound = useRecoilValue<number>(poundState);
+  const [input, setInput] = useState<string>(won.toString());
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value); // 입력값을 문자열로 저장
+  };
+
+  const handleConvertToDollar = () => {
+    const wonValue = Number(input); // 입력값을 숫자로 변환
+    if (!isNaN(wonValue)) {
+      setWon(wonValue); // 원화 상태를 업데이트
+    }
+  };
+
+  useEffect(() => {
+    setInput(won.toString()); // 원화 상태가 변할 때마다 입력값을 업데이트
+  }, [won]);
+
+  return (
+    <div className="bg-slate-600">
+      <h2 className="text-5xl text-center text-gray-300">💱 환전 계산기</h2>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+  <label className="text-3xl items-center text-black">
+    원화를 입력하세요:
+    <input
+      type="text"
+      value={input}
+      onChange={handleInputChange}
+      className="ml-2 border rounded px-2 py-1 bg-yellow-300"
+    />
+  </label>
+  <button
+    className="bg-red-100 text-red-800 mt-4 px-4 py-2 rounded"
+    onClick={handleConvertToDollar}
+  >
+    환전하기
+  </button>
+  <div className="text-blue-400 mt-4">
+    <p>💲 환전된 달러: ${dollar.toFixed(2)}</p>
+  </div>
+
+  <div className="text-purple-400 mt-4">
+    <p>💴 환전된 엔화: ￥{enhwa.toFixed(2)}</p>
+  </div>
+  <div className="text-green-400 mt-4">
+    <p>💷 환전된 파운드: ￡{pound.toFixed(2)}</p>
+  </div>
+</div>
+</div>
+  );
+};
+
+export default Exchange;
