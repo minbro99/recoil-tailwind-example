51b6a082557926205a89f2b4f590b883bfe3bb03
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-20 11:46:29 +0900

    Feat: 추가 빼기에 대한 states 추가
    counter.tsx에 쓰일 빼기에 대한 states 추가

--- recoil-tailwind-test1/src/states/CounterState.tsx
+++ recoil-tailwind-test1/src/states/CounterState.tsx
@@ -5,14 +5,31 @@ export const plusState = atom<number>({
     default: 0,
 });
 
-export const resultState = selector<number>({
-    key: "resultState",
+export const minusState = atom<number>({
+    key: "minusState",
+    default: 0,
+})
+
+export const plusResultState = selector<number>({
+    key: "plusResultState",
     get: ({ get }) => {
         const plus = get(plusState);  
         return plus + 1 ;  
     },
-    set: ({ set }, result) => {
-        const newPlusValue = (result as number) +1; 
+    set: ({ set }, result1) => {
+        const newPlusValue = (result1 as number) +1; 
         set(plusState, newPlusValue);  
+      },
+});
+
+export const minusResultState = selector<number>({
+    key: "minusResultState",
+    get: ({ get }) => {
+        const minus = get(minusState);  
+        return minus - 1 ;  
+    },
+    set: ({ set }, result2) => {
+        const newMinusValue = (result2 as number) -1; 
+        set(minusState, newMinusValue);  
       },
 })
