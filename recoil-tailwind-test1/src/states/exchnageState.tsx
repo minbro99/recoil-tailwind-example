0a7b2afd8858fe30d37acaf3d74ce92bbf530017
minbro99 <138020121+minbro99@users.noreply.github.com>
2024-08-20 14:01:49 +0900

    Feat: 엔화와 파운드에 대한 환전 states 추가
    엔화와 파운드에 대한 환전 states 추가

--- recoil-tailwind-test1/src/states/exchnageState.tsx
+++ recoil-tailwind-test1/src/states/exchnageState.tsx
@@ -2,19 +2,42 @@ import { atom, selector } from "recoil";
 
 // wonState의 타입은 number로 지정
 export const wonState = atom<number>({
-  key: "wonState",  // 고유한 키 값
-  default: 0,  // 기본값
+  key: "wonState",  
+  default: 0,  
 });
 
-// dollarState의 타입은 selector의 get 함수에서 반환하는 값과 set 함수에서 받을 값을 기반으로 결정
 export const dollarState = selector<number>({
-  key: "dollarState",  // 고유한 키 값
+  key: "dollarState",  
   get: ({ get }) => {
-    const won = get(wonState);  // wonState에서 현재 값을 가져옴
-    return won / 1400;  // 달러로 변환
+    const won = get(wonState);  
+    return won / 1400;  
   },
   set: ({ set }, dollar) => {
-    const newWonValue = (dollar as number) * 1400;  // dollar 값을 원으로 변환
-    set(wonState, newWonValue);  // wonState를 업데이트
+    const newWonValue = (dollar as number) * 1400; 
+    set(wonState, newWonValue);  
+  },
+});
+
+export const enhwaState = selector<number>({
+  key: "enhwaState",  
+  get: ({ get }) => {
+    const won = get(wonState);  
+    return won / 900;  
+  },
+  set: ({ set }, enhwa) => {
+    const newWonValue = (enhwa as number) * 900; 
+    set(wonState, newWonValue);  
+  },
+});
+
+export const poundState = selector<number>({
+  key: "poundState",  
+  get: ({ get }) => {
+    const won = get(wonState);  
+    return won / 1700;  
+  },
+  set: ({ set }, pound) => {
+    const newWonValue = (pound as number) * 1700; 
+    set(wonState, newWonValue);  
   },
 });
